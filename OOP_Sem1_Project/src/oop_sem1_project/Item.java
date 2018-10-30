6e2e89b59792c3ee6e902a95a8ba2e19af800c89
Frederik Hounsvad <36563020+Hounsvad@users.noreply.github.com>
2018-10-30 10:59:27 +0100

    Update Item.java

--- OOP_Sem1_Project/src/oop_sem1_project/Item.java
+++ OOP_Sem1_Project/src/oop_sem1_project/Item.java
@@ -11,65 +11,66 @@ import java.util.Objects;
  *
  * @author Hounsvad
  */
+
 public class Item {
+    
     /**
      * The name of the item
      * <p>Should only be one line
      */
     private final String itemName;
+    
     /**
      * A description of the item
      * <p>Can be multiple lines 
      */
     private final String itemDescription;
     
+    /**
+     * Creates an item
+     *
+     * @param itemName is the name of the item as a string
+     * @param itemDescription is the description of the item
+     * @throws IllegalArgumentException if the itemName is ""
+     * @throws NullPointerException if the 
+     */
     public Item(String itemName, String itemDescription){
-        if(itemName != null){
-            this.itemName = itemName;
-        }else if(itemName.equals("")){
-            throw new IllegalArgumentException("The input for itemName must be somthing other than \"\"");
-        }else{
-            throw new NullPointerException("The input for itemName must be somthing other than null");
-        }
         
-        if(itemDescription != null){
-            this.itemDescription = itemDescription;
-        }else if(itemDescription.equals("")){
-            throw new IllegalArgumentException("The input for itemDescription must be somthing other than \"\"");
-        }else{
+        if (itemName == null) {
             throw new NullPointerException("The input for itemName must be somthing other than null");
+        } else if (itemName.isEmpty()) {
+            throw new IllegalArgumentException("The input for itemName must be somthing other than \"\"");
         }
+        this.itemName = itemName;
+        
+        if (itemDescription == null) {
+            throw new NullPointerException("The input for itemName must be somthing other than null");
+        } else if (itemDescription.isEmpty()) {
+            throw new IllegalArgumentException("The input for itemDescription must be somthing other than \"\"");
+        }
+        this.itemDescription = itemDescription;
     }
 
     /**
-     * 
-     * @return 
+     * @return the item name
      */
     public String getItemName() {
-        return itemName;
+        return this.itemName;
     }
 
     /**
-     * 
-     * @return 
+     * @return the item description
      */
     public String getItemDescription() {
-        return itemDescription;
+        return this.itemDescription;
     }
     
+    /**
+     * Checks if the itemName is equal
+     * @return true upon equality
+     */
     @Override
     public boolean equals(Object input){
-        if(input instanceof Item){
-            return ((Item) input).itemName.equalsIgnoreCase(this.itemName);
-        }
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 79 * hash + Objects.hashCode(this.itemName);
-        hash = 79 * hash + Objects.hashCode(this.itemDescription);
-        return hash;
+        return input instanceof Item && ((Item) input).itemName.equalsIgnoreCase(this.itemName);
     }
 }
