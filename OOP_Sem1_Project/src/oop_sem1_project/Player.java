3ff8554e42abc8609a104bb9e465df948a76084d
Oliver Nordestgaard <olnor18@student.sdu.dk>
2018-10-30 10:44:06 +0100

    Player - now with javadoc

--- OOP_Sem1_Project/src/oop_sem1_project/Player.java
+++ OOP_Sem1_Project/src/oop_sem1_project/Player.java
@@ -14,26 +14,54 @@ public class Player {
     private String name;
     private final Inventory inventory = new Inventory();
     
+    /**
+     *
+     * @param name
+     */
     public Player(String name) {
         this.name = name;
     }
     
+    /**
+     *
+     * @return The name of the player
+     */
     public String getName(){
         return name;
     }
     
+    /**
+     *
+     * @param item
+     * Adds said item
+     */
     public void addItem(Item item){
         inventory.addItem(item);
     }
     
+    /**
+     *
+     * @param itemName
+     * Removes said item
+     */
     public void removeItem(String itemName){
         inventory.removeItem(itemName);
     }
     
+    /**
+     *
+     * @param itemName
+     * @return Item with that name
+     */
     public Item getItem(String itemName){
         return inventory.getItem(itemName);
     }
     
+    /**
+     *
+     * @param itemName
+     * @return Whether it has that item as a boolean
+     */
     public boolean hasItem(String itemName){
         return inventory.hasItem(itemName);
     }
