31e200e5de4106856b13d5383f250439e6b7f918
Frederik Hounsvad <Frederik@hounsvad.dk>
2018-11-06 13:20:13 +0100

    Fixe lé mapé we we

--- OOP_Sem1_Project/src/oop_sem1_project/items/SafetyPoint.java
+++ OOP_Sem1_Project/src/oop_sem1_project/items/SafetyPoint.java
@@ -5,6 +5,7 @@
  */
 package oop_sem1_project.items;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,19 +29,19 @@ public class SafetyPoint {
                 + "#      U55      #      Hall      #    Workshop     #\n"
                 + "#               #                #                 #\n"
                 + "####################################################\n"
-                + "                 #               #\n"
-                + "                 #   Entrance    #\n"
-                + "                 #               #\n"
-                + "                 #################\n"
-                + "                 #               #\n"
-                + "                 #  Rally Point  #\n"
-                + "                 #               #\n"
-                + "                 #################"));
-        CONTENTS.put("fire-extinguisher", new Item("Fire-extinguisher", "A CO2 filled vessel with a release handle", "*SPWOOOOOOSH!* Goes the fire-extinguisher"));
-        CONTENTS.put("eyewash", new Item("Eyewash", "A saline solution in a handy container", "*SPLASH* Goes the eyewash"));
-        CONTENTS.put("defibrilator", new Item("Defibrilator", "A medicaly approved arc-reactor-powered tazer in red and gold colour scheme", "*BZZZZZT* Goes the fancy tazer"));
-        CONTENTS.put("first-aid", new Item("First-aid", "A bag of old socks", "As the socks are stuffed into the wound the patient screams in agony"));
-        CONTENTS.put("e-phone", new Item("E-Phone", "A Nokia E90 Communicator", "As you call 65 50 88 88 you hear \"You are number (*Error overflow*) in the cue\""));
+                + "                #                #\n"
+                + "                #    Entrance    #\n"
+                + "                #                #\n"
+                + "                ##################\n"
+                + "                #                #\n"
+                + "                #   Rally Point  #\n"
+                + "                #                #\n"
+                + "                ##################", Arrays.asList(0)));
+        CONTENTS.put("fire-extinguisher", new Item("Fire-extinguisher", "A CO2 filled vessel with a release handle", "*SPWOOOOOOSH!* Goes the fire-extinguisher", Arrays.asList(0)));
+        CONTENTS.put("eyewash", new Item("Eyewash", "A saline solution in a handy container", "*SPLASH* Goes the eyewash", Arrays.asList(0)));
+        CONTENTS.put("defibrilator", new Item("Defibrilator", "A medicaly approved arc-reactor-powered tazer in red and gold colour scheme", "*BZZZZZT* Goes the fancy tazer", Arrays.asList(0)));
+        CONTENTS.put("first-aid", new Item("First-aid", "A bag of old socks", "As the socks are stuffed into the wound the patient screams in agony", Arrays.asList(0)));
+        CONTENTS.put("e-phone", new Item("E-Phone", "A Nokia E90 Communicator", "As you call 65 50 88 88 you hear \"You are number (*Error overflow*) in the cue\"", Arrays.asList(0)));
     }
 
     /**
