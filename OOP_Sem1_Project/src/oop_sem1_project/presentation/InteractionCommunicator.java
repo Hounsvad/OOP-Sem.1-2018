0323ef5e5b6217e5d138b0a76fdf5cee194c1d32
Benjamin Staugaard <bemmamin@live.dk>
2018-11-21 11:23:26 +0100

    JavaDoc

--- OOP_Sem1_Project/src/oop_sem1_project/presentation/InteractionCommunicator.java
+++ OOP_Sem1_Project/src/oop_sem1_project/presentation/InteractionCommunicator.java
@@ -16,18 +16,37 @@ import oop_sem1_project.domain.InteractionHandler;
  */
 public class InteractionCommunicator {
 
+    /**
+     * InteractionHandler.
+     */
     private final InteractionHandler interactionHandler;
 
+    /**
+     * Constructs a new InteractionCommunicator which instantiates a new
+     * InteractionHandler.
+     */
     public InteractionCommunicator() {
         this.interactionHandler = new InteractionHandler();
     }
 
+    /**
+     * Called when any key is pressed. If the key is WASD or an arrow key it is
+     * handled in the domain layer.
+     *
+     * @param keycode The code of the key pressed.
+     */
     public void keyEvent(KeyCode keycode) {
         if (Arrays.asList(KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.UP, KeyCode.DOWN, KeyCode.LEFT, KeyCode.RIGHT).contains(keycode)) {
             this.interactionHandler.keyEvent(keycode);
         }
     }
 
+    /**
+     * Called when the Game-, Phone- or Item Canvas is clicked.
+     *
+     * @param clickedNode The clicked canvas.
+     * @param mouseEvent The MouseEvent.
+     */
     public void mouseClickedEvent(ClickedNode clickedNode, MouseEvent mouseEvent) {
         interactionHandler.mouseClick(clickedNode, mouseEvent);
     }
