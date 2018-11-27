667c5f3585f59914fc66984a6622336d84e40f1e
Benjamin Staugaard <bemmamin@live.dk>
2018-11-27 22:32:09 +0100

    Renderer

---
+++ OOP_Sem1_Project/src/oop_sem1_project/presentation/Renderer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop_sem1_project.presentation;
+
+
+/**
+ *
+ * @author Benjamin Staugaard | Benz56
+ */
+public class Renderer {
+
+    private final WOSController controller;
+
+    public Renderer(WOSController controller) {
+        this.controller = controller;
+    }
+
+}
