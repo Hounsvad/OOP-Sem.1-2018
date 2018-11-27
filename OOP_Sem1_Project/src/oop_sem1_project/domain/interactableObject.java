66e8fd9d30bcce1825b55a19fd792284e003b5a5
Frederik Hounsvad <Frederik@hounsvad.dk>
2018-11-28 00:33:51 +0100

    Completion of InteractableObject class

--- OOP_Sem1_Project/src/oop_sem1_project/domain/interactableObject.java
+++ OOP_Sem1_Project/src/oop_sem1_project/domain/interactableObject.java
@@ -9,6 +9,36 @@ package oop_sem1_project.domain;
  *
  * @author Pinnacle F
  */
-public class interactableObject {
+public class interactableObject extends DisplayableUnit{
+
+    private int[] clickableArea;
+
+    public interactableObject(int[] clickableArea) {
+        /*Insert super call and fix signature*/
+        this.clickableArea = clickableArea;
+    }
+
     
+    
+    /**
+     * Checks to see if the coordinates are withing the clickable area
+     *
+     * @param x as an int to signify the x coordinate of the clicked point
+     * @param y as an int to signify the y coordinate of the clicked point
+     * @return true if the input is within the clickable area
+     */
+    public boolean isInArea(int x, int y) {
+        if (x > 0 && y > 0 && x >= clickableArea[0] && x <= clickableArea[2] && y > clickableArea[1] && y <= clickableArea[3]) {
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     *
+     * @return the position of the object
+     */
+    public int[] getPosition() {
+        return position;
+    }
 }
