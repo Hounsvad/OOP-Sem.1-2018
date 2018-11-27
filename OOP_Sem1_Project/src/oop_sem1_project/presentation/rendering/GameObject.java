08a905a9dc65620f7ab1ca48afa69f6badf7cb13
Benjamin Staugaard <bemmamin@live.dk>
2018-11-27 23:11:16 +0100

    Rough Renderer Implementation.

---
+++ OOP_Sem1_Project/src/oop_sem1_project/presentation/rendering/GameObject.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop_sem1_project.presentation.rendering;
+
+/**
+ *
+ * @author Benjamin Staugaard | Benz56
+ */
+class GameObject {
+
+    /**
+     * The name of the GameObject's image.
+     */
+    private final String image;
+
+    /**
+     * The x coordinate of the GameObject.
+     */
+    private final int x;
+
+    /**
+     * The y coordinate of the GameObject.
+     */
+    private final int y;
+
+    /**
+     * Construct a new GameObject.
+     *
+     * @param image The image.
+     * @param x The x coordinate.
+     * @param y The y coordinate.
+     */
+    public GameObject(String image, int x, int y) {
+        this.image = image;
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * @return The image name.
+     */
+    public String getImage() {
+        return this.image;
+    }
+
+    /**
+     * @return The x coordinate.
+     */
+    public int getX() {
+        return this.x;
+    }
+
+    /**
+     * @return The y coordinate.
+     */
+    public int getY() {
+        return this.y;
+    }
+}
---
+++ OOP_Sem1_Project/src/oop_sem1_project/presentation/rendering/Renderer.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop_sem1_project.presentation.rendering;
+
+import java.util.List;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import oop_sem1_project.presentation.WOSController;
+
+/**
+ *
+ * @author Benjamin Staugaard | Benz56
+ */
+public class Renderer {
+
+    /**
+     * The image of the player.
+     */
+    private final Image player = new Image(getClass().getResourceAsStream("images/player.png"));
+
+    /**
+     * An instance of the WOSController to access the canvases.
+     */
+    private final WOSController controller;
+
+    /**
+     * Construct a new Renderer.
+     *
+     * @param controller The instance of the WOSController.
+     */
+    public Renderer(WOSController controller) {
+        this.controller = controller;
+    }
+
+    /**
+     * Re-draws everything on the canvas based on the received dataList.
+     *
+     * @param dataList The list that specifies where something should be drawn.
+     */
+    public void requestGraphicalUpdate(List<String[]> dataList) {
+        // Main purpose of the RenderingDataReader is to convert the dataList to something more specific
+        // than arbritray abbrevations and random array positions. Aids readability.
+        RenderingDataReader dataReader = new RenderingDataReader(dataList);
+        GraphicsContext gameCanvas = this.controller.getGameCanvas().getGraphicsContext2D();
+
+        gameCanvas.drawImage(new Image(getClass().getResourceAsStream("images/" + dataReader.getBackground() + ".png")), 0, 0);
+        if (dataReader.drawPlayer()) {
+            gameCanvas.drawImage(this.player, dataReader.getPlayerX(), dataReader.getPlayerY());
+        }
+
+        for (GameObject gameObject : dataReader.getGameObjects()) {
+            gameCanvas.drawImage(new Image(getClass().getResourceAsStream("images/" + gameObject.getImage() + ".png")), gameObject.getX(), gameObject.getY());
+        }
+
+        this.controller.getItemCanvas().getGraphicsContext2D().drawImage(new Image(getClass().getResourceAsStream("images/" + dataReader.getCurrentItem() + ".png")), 0, 0);
+    }
+}
