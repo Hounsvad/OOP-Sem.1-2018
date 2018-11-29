c0bc5da7a6db5f8c4fec62d001995b22201fe229
Patrick Nielsen <prik.n@hotmail.de>
2018-11-29 12:03:12 +0100

    Set desired progress to -1000 default messages

--- OOP_Sem1_Project/src/oop_sem1_project/domain/GameContainer.java
+++ OOP_Sem1_Project/src/oop_sem1_project/domain/GameContainer.java
@@ -38,38 +38,38 @@ public class GameContainer {
         hall.addMessage(11, "Flee you fool!");
 
         Room hallway = new Room("Hallway", new int[]{0, 0}, "Hallway", "This is a hallway adjacent to the mainhall", 1, false);
-        hallway.addMessage(0, "As you walk along the empty hallway, the smell of burnt electronics gets stronger.");
+        hallway.addMessage(-1000, "As you walk along the empty hallway, the smell of burnt electronics gets stronger.");
         hallway.addMessage(1, "There is still a burning smell in the air. \nYou should find out where its coming from.");
         hallway.addMessage(2, "As you walk in to the hallway you hear a scream coming from the chemestry lab.");
         hallway.addMessage(3, "The screams are still ringing out of the chemestry lab.");
         hallway.addMessage(4, "The hallway appears to be empty.");
 
         Room robtek = new Room("Rob-tek", new int[]{0, 0}, "RobTek", "This is the room, the robottechnology enginneers reside in", 2, true);
-        robtek.addMessage(0, "There is honestly no reason to ever be here");
+        robtek.addMessage(-1000, "There is honestly no reason to ever be here");
         robtek.addMessage(1, "A burning smell enters your nose. In the corner of the room you see flames shooting out of a one-armed robot.\nOne of the engineers shouts: \"Someone please help! We are trapped behind the fire spewing robot\".");
         robtek.addMessage(2, "They really can't put out this fire on their own");
         robtek.addMessage(3, "Even with the fire gone, it still smells here \nBetter move on");
 
         Room chemlab = new Room("Chem-lab", new int[]{0, 0}, "ChemLab", "A Chemical Laboratory", 4, true);
-        chemlab.addMessage(0, "You walk up to the door of the Chemical lab, a sign on it says: \n \"Stay out, experiment in progress\"");
+        chemlab.addMessage(-1000, "You walk up to the door of the Chemical lab, a sign on it says: \n \"Stay out, experiment in progress\"");
         chemlab.addMessage(1, "One of the chemists, screams in agony while desperatly rubbing his hands into his eyes \nYou have to help him");
         chemlab.addMessage(2, "The chemist is still screaming \nHe could use some help");
         chemlab.addMessage(3, "The sign still says \"Stay out, experiment in progress\" \n They are hopefully wearing safety glasses now");
 
         Room workshop = new Room("Workshop", new int[]{0, 0}, "Workshop", "A Workshop for mechanical engineering", 6, true);
-        workshop.addMessage(0, "You enter the workshop \n The air is filled with sawdust \n There is nothing to see here.");
+        workshop.addMessage(-1000, "You enter the workshop \n The air is filled with sawdust \n There is nothing to see here.");
         workshop.addMessage(1, "You notice blood pouring from one of the student's arms. \n\"Tis but a scratch!\" he claims. \nYou should probably help him anyway...");
         workshop.addMessage(2, "The Student is still calm. \nThe blood puddle on the floor seems to get larger with every second");
         workshop.addMessage(3, "The cleaning ladies are trying to get rid of all the blood on the floor \nYou dont want to bother them");
 
         Room u55 = new Room("U55", new int[]{0, 0}, "U55", "This is one of the lecturehall of the university", 8, true);
-        u55.addMessage(0, "Someone is giving a lecture right now, you decide to stay and listen for a while \n \" ...og som i har læst i e-tivitet 4... \" \n You decide not to listen any more and decide to head out of the room.");
+        u55.addMessage(-1000, "Someone is giving a lecture right now, you decide to stay and listen for a while \n \" ...og som i har læst i e-tivitet 4... \" \n You decide not to listen any more and decide to head out of the room.");
         u55.addMessage(1, "In the middle of all the commotion, you notice a student in the back, laying on the floor! Someone is allready doing cpr.");
         u55.addMessage(2, "The student lying on the floor doesn't seem to be responsive and its allready been a little while \nYou should act quick!");
         u55.addMessage(3, "Everyone seems to have calmed down already, some students even fell asleep...");
 
         Room rallypoint = new Room("Rallypoint", new int[]{0, 0}, "Rallypoint", "This is the Rallypoint where you're supposed to meet up in case of emergency", 11, true);
-        rallypoint.addMessage(0, "Why would you leave already? The day isn't over");
+        rallypoint.addMessage(-1000, "Why would you leave already? The day isn't over");
         rallypoint.addMessage(1, "Everyone gasps at the sight of the TEK building burning to ashes. \nAt least all the students are safe \n\"Wait! Has anyone seen the Rob-tek students?\"");
         
         rooms.add(hall);
