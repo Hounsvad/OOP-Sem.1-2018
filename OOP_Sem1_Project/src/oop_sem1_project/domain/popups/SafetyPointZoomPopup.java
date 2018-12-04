6b9a2389e43ff324eb345929d9b3aa160c34e1d5
Patrick <prik.n@hotmail.de>
2018-12-04 22:09:38 +0100

    Even still fixing SafetyPoint areas

--- OOP_Sem1_Project/src/oop_sem1_project/domain/popups/SafetyPointZoomPopup.java
+++ OOP_Sem1_Project/src/oop_sem1_project/domain/popups/SafetyPointZoomPopup.java
@@ -15,10 +15,10 @@ public class SafetyPointZoomPopup extends Popup {
 
     public SafetyPointZoomPopup(InteractionHandlerImpl interactionHandler, String name, String image) {
         super(interactionHandler, name, image);
-        
+
         addClickableAreas("exit1", new int[]{0, 0, 164, 500});
         addClickableAreas("exit2", new int[]{735, 0, 165, 500});
-        addClickableAreas("exit3", new int[]{0,0, 900, 83});
+        addClickableAreas("exit3", new int[]{0, 0, 900, 83});
         addClickableAreas("exit4", new int[]{386, 0, 900, 114});
     }
 
@@ -30,7 +30,7 @@ public class SafetyPointZoomPopup extends Popup {
             case "exit2":
             case "exit3":
             case "exit4":
-            getInteractionHandler().getGameContainer().setPopup(null);
+                getInteractionHandler().getGameContainer().setPopup(new SafetyPointClosedPopup(getInteractionHandler(), "SafetyPointClosed", "SafetyPointClosed"));
         }
     }
 }
