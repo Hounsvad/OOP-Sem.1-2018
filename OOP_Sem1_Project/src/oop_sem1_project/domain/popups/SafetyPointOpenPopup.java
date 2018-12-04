3d6146dc6c3a58f45fe370e9ec8bafc712ed089a
Patrick <prik.n@hotmail.de>
2018-12-04 22:09:26 +0100

    Still fixing SafetyPoint areas

--- OOP_Sem1_Project/src/oop_sem1_project/domain/popups/SafetyPointOpenPopup.java
+++ OOP_Sem1_Project/src/oop_sem1_project/domain/popups/SafetyPointOpenPopup.java
@@ -13,35 +13,28 @@ import oop_sem1_project.domain.InteractionHandlerImpl;
  */
 public class SafetyPointOpenPopup extends Popup {
 
-
-
     SafetyPointOpenPopup(InteractionHandlerImpl interactionHandler, String name, String image) {
         super(interactionHandler, name, image);
-    
-    addClickableAreas("map", new int[]{366, 117, 97, 92});
-    addClickableAreas("whatToDo", new int[]{474, 119, 54, 71});
-    addClickableAreas("fire-extinguisher", new int[]{363, 318, 68, 153});
-    addClickableAreas("defibrilator", new int[]{457, 321, 64, 56});
-    addClickableAreas("first-aid", new int[]{458, 404, 66, 57});
-    addClickableAreas("eyewash", new int[]{474, 119, 54, 71}); //add Eyewash
-    addClickableAreas("exit1", new int[]{0,0, 343, 500});
-    addClickableAreas("exit2", new int[]{557, 0, 343, 500});
-    }
 
+        addClickableAreas("zoom", new int[]{366, 117, 163, 97});
+        addClickableAreas("fire-extinguisher", new int[]{363, 318, 68, 153});
+        addClickableAreas("defibrilator", new int[]{461, 340, 61, 56});
+        addClickableAreas("first-aid", new int[]{458, 403, 66, 59});
+        addClickableAreas("eyewash", new int[]{458, 329, 64, 29});
+        addClickableAreas("exit1", new int[]{0, 0, 343, 500});
+        addClickableAreas("exit2", new int[]{557, 0, 343, 500});
+    }
 
     @Override
     public void onClick(int[] clickedPosition) {
         String clickedArea = getClickedArea(clickedPosition);
         if (clickedArea != null) {
             switch (clickedArea) {
-                case "map":
-                    getInteractionHandler().getGameContainer().setPopup(new SafetyPointZoomPopup(getInteractionHandler(), "ZoomMap", "SafetyPointMap"));
-                    break;
-                case "whatToDo":
-                    getInteractionHandler().getGameContainer().setPopup(new SafetyPointZoomPopup(getInteractionHandler(), "ZoomWhatToDo", "SafetyPointWhatToDo"));
+                case "zoom":
+                    getInteractionHandler().getGameContainer().setPopup(new SafetyPointZoomPopup(getInteractionHandler(), "ZoomMap", "SafetyPointOverlay"));
                     break;
                 case "fire-extinguisher":
-                    getInteractionHandler().getGameContainer().getPlayer().setItem(getInteractionHandler().getGameContainer().getItems().get(0)); //Add ItemList to GameContainer
+                    getInteractionHandler().getGameContainer().getPlayer().setItem(getInteractionHandler().getGameContainer().getItems().get(0));
                     break;
                 case "defibrilator":
                     getInteractionHandler().getGameContainer().getPlayer().setItem(getInteractionHandler().getGameContainer().getItems().get(1));
