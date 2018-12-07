8321ef5d2b4162186a48e8b2236a4e97625ce1d2
Unknown <anlau15@student.sdu.dk>
2018-12-07 11:07:02 +0100

    kk

--- OOP_Sem1_Project/src/oop_sem1_project/domain/GameContainer.java
+++ OOP_Sem1_Project/src/oop_sem1_project/domain/GameContainer.java
@@ -145,6 +145,7 @@ public class GameContainer {
         rallypoint.addInteractableArea("doorNorth", new Door("doorNorth", new int[]{400, 0}, new int[]{50, 0}, 50, "door", null, entrance, true));
         rallypoint.addInteractableArea("quiz", new InteractableArea(new int[]{400, 300}, new int[]{100, 100}, 0, "quiz", null));
         
+        
         rooms.addAll(Arrays.asList(hall, hallway, robtek, chemlab, workshop, u55, rallypoint));
         this.player.setCurrentRoom(entrance);
 
