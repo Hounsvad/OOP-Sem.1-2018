069412c379e90e18ec0e8275f00dc2a0705d45fd
Benjamin Staugaard <bemmamin@live.dk>
2018-12-11 15:59:04 +0100

    Sound fix.

--- OOP_Sem1_Project/src/oop_sem1_project/presentation/audio/WOSMediaPlayer.java
+++ OOP_Sem1_Project/src/oop_sem1_project/presentation/audio/WOSMediaPlayer.java
@@ -1,11 +1,6 @@
 package oop_sem1_project.presentation.audio;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
 import java.net.URISyntaxException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
